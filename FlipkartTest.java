6ebd2b18ef5cf5c668999c8a04a00d5c3b1c0c2e
HaripriyaVemula <65009238+HaripriyaVemula@users.noreply.github.com>
2024-01-07 14:20:14 +0530

    Create  FlipkartTest.java

---
+++ FlipkartTest.java
@@ -0,0 +1,18 @@
+package haripriyapackage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.chromedriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+ public class FlipkartTest extends BaseTest {
+
+    @Test
+    public void addToCartTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.searchProduct("Macbook air m2");
+        ProductPage productPage = homePage.clickFirstProduct().assertPageTitle();
+        productPage.addToCart();
+        CartPage cartPage = new CartPage(driver);
+        cartPage.verifyItemAdded();
+    }
+}
