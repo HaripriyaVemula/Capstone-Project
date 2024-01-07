9d395c25cd557559cbbcb679a369d0359597b481
HaripriyaVemula <65009238+HaripriyaVemula@users.noreply.github.com>
2024-01-07 14:24:03 +0530

    Create test_flipkart.py

---
+++ pythonproject/test_flipkart.py
@@ -0,0 +1,11 @@
+import pytest
+from pages import HomePage, ProductPage, CartPage
+
+@pytest.mark.usefixtures("driver")  # Use the driver fixture
+class TestFlipkart:
+    def test_add_to_cart(self, driver):
+        home_page = HomePage(driver)
+        home_page.search_product("Macbook air m2")
+        product_page = home_page.click_first_product().assert_page_title()
+        cart_page = product_page.add_to_cart()
+        cart_page.verify_item_added()
